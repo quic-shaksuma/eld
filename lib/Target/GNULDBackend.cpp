64384249bcd45681f23b1cb8d42b33b322ca2fcd
Parth Arora <partaror@qti.qualcomm.com>
2025-11-24 19:04:58 +0530

    Evaluate output section end symbol assignments in partial link
    Until now, the linker was not evaluating output section end symbol
    assignments in partial links. This commit fixes this behavior.
    
    Resolves #479
    
    Signed-off-by: Parth Arora <partaror@qti.qualcomm.com>

--- lib/Target/GNULDBackend.cpp
+++ lib/Target/GNULDBackend.cpp
@@ -2681,6 +2681,7 @@ bool GNULDBackend::setOutputSectionOffset() {
     if (isCurAlloc && cur->size())
       changeSymbolsFromAbsoluteToGlobal(*out);
     prev = cur;
+    evaluateAssignmentsAtEndOfOutputSection(*out);
     ++out;
   }
   return true;
---
+++ test/Common/standalone/linkerscript/PartialLinkWithOutputSectionEndAssignments/Inputs/1.c
@@ -0,0 +1,3 @@
+extern int u1;
+int foo() { return 1; }
+int bar() { return u1; }
