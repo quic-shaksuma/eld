1cef6696fb42865bbe623f8f70df3ba0551575ca
quic-areg <aregmi@qti.qualcomm.com>
2026-03-05 12:40:59 -0800

    [test] Fix debug string tests after compiler change
    Common/standalone/DebugStrRelocs/debugstrrelocs.test started failing
    after upstream change 63074da2 put global-variable DWARF DIEs later.
    The test has duplicates/near-duplicates that are now deleted, and the
    test is made order-insensitive to pass.
    
    Closes #912.
    
    Signed-off-by: quic-areg <aregmi@qti.qualcomm.com>

--- test/Common/standalone/DebugStrCorruption/Inputs/1.c
+++
@@ -1,2 +0,0 @@
-const int a = 0;
-__attribute__((weak)) int bar() { return a; }
--- test/Hexagon/standalone/DebugStrCorruption/Inputs/2.c
+++
@@ -1,3 +0,0 @@
-const int b = 0;
-__attribute__((weak)) int foo() { return b; }
-__attribute__((weak)) int bar() { return b; }
